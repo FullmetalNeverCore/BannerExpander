09d1ceaeadd5449131205860104457451b29bd45
FullmetalCoder <60969891+FullmetalNeverCore@users.noreply.github.com>
2022-06-21 08:29:26 +0300

    Add files via upload

--- converter.py
+++ converter.py
@@ -1,67 +1,62 @@
 import os 
-import sys 
-import ujson
+import sys
+from telnetlib import X3PAD 
+import json
 from numba import njit,prange
 import math 
 import random 
+import calendar
+import datetime 
 
-
-def open_file(x):
-    f = open(x)
-    data = ujson.load(f)
-    return [data[x] for x in range(0,len(data)) if data[x].get("bannerType") == "WEAPON"],[data[x] for x in range(0,len(data)) if data[x].get("bannerType") == "EVENT"]
-
+def open_file(x,mode):
+    if mode == 1 or mode == 2:
+        f = open(x)
+        data = json.load(f)
+    if mode == 1:
+        return [data[x] for x in range(0,len(data)) if data[x].get("bannerType") == "WEAPON"]
+    elif mode == 2:
+        return [data[x] for x in range(0,len(data)) if data[x].get("bannerType") == "EVENT"]
 
 
 
 class Change_Value():
 
-    def __init__(self,char,weap):
-        self.initial_time = 1659999922
-        self.initial_weapon_time = 1659999922
-        self.ch = char
-        self.wp = weap
-        for x in range(1,8):
-            for x in range(0,len(self.ch)):
-                self.ch.append(self.ch[x])
-            for x in range(0,len(self.wp)):
-                self.ch.append(self.wp[x])
-        self.event_banner = self.change_values_char()
-        self.weapon_banner = self.change_values_weap()
-        self.final_banner_list = self.event_banner+self.weapon_banner
-        self.write_file(self.event_banner+self.weapon_banner)
+    def __init__(self):
+        self.curr_time = calendar.timegm((datetime.datetime.utcnow()).timetuple())
+        print(self.curr_time)
+        self.change_values_char(open_file("Banners.json",2),1)
+        print("Ready!")
 
-    def change_values_char(self):
-        print(len(self.ch))
-        print("START CHAR")
-        #waeaponbanner = [data_object[x] for x in range(0,len(data_object)) if data_object[x].get("bannerType") == "WEAPON"]
-        for x in range(0,len(self.ch)):
-                self.ch[x].update(beginTime=int(self.initial_time),endTime=int(self.initial_time+1000))
-                #chosen_weapon = random.choice(waeaponbanner)
-                #data_object.append(chosen_weapon)
-                self.initial_time = self.initial_time + 1000
-                print(str(self.initial_time) + "Char")
-        #initial_time = 1650661019
-        #for x in range(0,len(data_object)):
-            #if(data_object[x].get("bannerType") == "WEAPON"):
-               # data_object[x].update(beginTime=int(initial_time),endTime=int(initial_time+1000))
-               # initial_time += 1000
+    def change_values_char(self,x,mode):
+        minutes = 30
+        self.prev_x = x
+        self.ch = x
+        #for x in range(2):
+            #for x in range(0,len(self.ch)):
+                    #self.ch.append(self.ch[x])
+        if mode != 1:
+            for x in self.ch:
+                future = calendar.timegm((datetime.datetime.fromtimestamp(self.curr_time)  + datetime.timedelta(minutes=int(minutes))).timetuple())
+                x["beginTime"] = self.curr_time
+                x["endTime"] = future
+                self.curr_time = calendar.timegm((datetime.datetime.fromtimestamp(self.curr_time)  + datetime.timedelta(minutes=int(minutes))).timetuple())
+                minutes += 30
+            with open("Banner3.json","w") as of:
+                json.dump(self.ch,of,indent=2)
+        else:
+            for x in self.ch:
+                future = calendar.timegm((datetime.datetime.fromtimestamp(self.curr_time)  + datetime.timedelta(minutes=int(minutes))).timetuple())
+                x["beginTime"] = self.curr_time
+                x["endTime"] = future
+                self.curr_time = calendar.timegm((datetime.datetime.fromtimestamp(self.curr_time)  + datetime.timedelta(minutes=int(minutes))).timetuple())
+                minutes += 30
+            self.final_banner = self.ch + self.prev_x
+            with open("Banner3.json","w") as of:
+                json.dump(self.final_banner,of,indent=2)
 
-        #print(data_object)
-        return self.ch
+            
+
+
     
-    def change_values_weap(self):
-        print(len(self.wp))
-        print("START WEAPON")
-        for x in range(0,len(self.wp)):
-            if(self.wp[x].get("bannerType") == "WEAPON"):
-                self.wp[x].update(beginTime=int(self.initial_weapon_time),endTime=int(self.initial_weapon_time+1000))
-                self.initial_weapon_time = self.initial_weapon_time + 1000
-        return self.wp
-    
-    def write_file(self,x):
-        with open('Banner3.json', 'w') as outfile:outfile.write(ujson.dumps(x,indent=2))
 
-
-x,y = open_file("Banners.json")
-Change_Value(x,y)
+Change_Value()
